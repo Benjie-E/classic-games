34dc3419975a3a62a4a51819c6fe552e11fd4698
John <john.groton@mymail.champlain.edu>
2023-02-23 16:13:57 -0500

    more work on cmake

---
+++ Cmake Build/CMakeFiles/pdcurses-config.cmake
@@ -0,0 +1,3 @@
+# Auto-generated CMake config file for jackoalan/pdcurses package
+cmake_minimum_required(VERSION 2.8)
+include("C:/Users/jgrot/OneDrive/Desktop/classic-games/Cmake Build/CMakeFiles/pdcurses-targets.cmake")
