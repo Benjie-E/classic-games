7bcf44f32144adfb7c9267d8801d1b6090776c29
Benjie Eisen <90060865+Benjie-E@users.noreply.github.com>
2023-02-27 15:35:23 -0500

    added input getter

--- SnakeCode/Snake/Snake/Snake.cpp
+++ SnakeCode/Snake/Snake/Snake.cpp
@@ -1,8 +1,41 @@
+#include <curses.h>
+#include "Snake.h"
 #include <iostream>
 
+Snake::Snake() {
+
+}
+int Snake::getSnakeInput() {
+    int input = getch();
+    if (input == -1) {
+        return -1;
+    }
+    switch (input) {
+        case KEY_RIGHT:
+            return 0;
+            break;
+        case KEY_DOWN:
+            return 1;
+            break;
+        case KEY_LEFT:
+            return 2;
+            break;
+        case KEY_UP:
+            return 3;
+            break;
+        default:
+            return -1;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    initscr();
+    noecho();
+    keypad(stdscr,true);
+    Snake snek;
+    while (!false) {
+        snek.getSnakeInput();
 
+    }
+}
---
+++ SnakeCode/Snake/Snake/Snake.h
@@ -0,0 +1,7 @@
+#pragma once
+
+class Snake {
+public:
+	Snake();
+	int getSnakeInput(); //RDLU
+};
