88dd734f7191b1747216f8c178893cf5d2a9083b
Benjie Eisen <90060865+Benjie-E@users.noreply.github.com>
2023-04-04 10:34:33 -0400

    added path header and updated to C++ v20

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include "GameSelector.h"
-int main() {
+#include "path.h"
+#include <iostream>
+int main(int argc, char** argv) {
+	std::filesystem::path exePath = std::filesystem::absolute(std::filesystem::path(argv[0])); //get path to executable
 	GameSelector gameSelector;
 	while (true) {
 		gameSelector.start(gameSelector.select());
